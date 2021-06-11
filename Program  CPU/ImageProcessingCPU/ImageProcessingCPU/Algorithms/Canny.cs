6f2c80bac0949d3c8ce9aa05dc677a1f6f25881d
LatentVirtue <aleksandar1177@gmail.com>
2021-06-11 19:35:12 +0200

    Updated gradient angle and intensity calculations

--- Program  CPU/ImageProcessingCPU/ImageProcessingCPU/Algorithms/Canny.cs
+++ Program  CPU/ImageProcessingCPU/ImageProcessingCPU/Algorithms/Canny.cs
@@ -13,8 +13,9 @@ namespace ImageProcessingCPU.Algorithms
     class Canny : IAlgoInterface
     {
         Image actual;
-        double[,] angle;
+        int[,] angle;
         double[,] gIntensity;
+        double adj = 1;
         public Canny()
         {
 
@@ -39,15 +40,14 @@ namespace ImageProcessingCPU.Algorithms
             int[,] sobelX = { { 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 } };
             int[,] sobelY = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
             //applying kernels
-            Bitmap Gx = new Bitmap(ImageHandler.factory.Image);
-            Bitmap Gy = new Bitmap(ImageHandler.factory.Image);
-            Gx = Convolve(Gx, ref sobelX);
-            Gy = Convolve(Gy, ref sobelY);
+            Bitmap temp = new Bitmap(actual);
+            int[,] Gx = Convolve(ref temp, ref sobelX);
+            int[,] Gy = Convolve(ref temp, ref sobelY);
             //computing gradient intensity via G = sqrt(Gx^2+Gy^2)
-            ComputeGradient(Gx, Gy);
+            ComputeGradient(ref Gx, ref Gy);
 
             //computing gradient angle - needed for tresholding
-            angle = GradientDirection(Gx, Gy);
+            GradientDirection(ref Gx, ref Gy);
             //non-max suppresion
             NonMaxSuppression();
             actual = ReconstructFromGradient();
@@ -55,62 +55,110 @@ namespace ImageProcessingCPU.Algorithms
             ImageHandler.factory.Load(actual);
         }
         //also implement channels
-        void ComputeGradient(Bitmap Gx, Bitmap Gy)
+        void ComputeGradient(ref int[,] Gx, ref int[,] Gy)
         {
-            gIntensity = new double[Gx.Height, Gx.Width];
+            gIntensity = new double[Gx.GetLength(0), Gx.GetLength(1)];
             for (int i = 0; i < gIntensity.GetLength(0); i++)
             {
                 for (int j = 0; j < gIntensity.GetLength(1); j++)
                 {
-                    Color tX = Gx.GetPixel(j, i);
-                    Color tY = Gy.GetPixel(j, i);
-                    gIntensity[i, j] = Math.Sqrt(tX.R * tX.R + tY.R * tY.R);
+                    gIntensity[i, j] = Math.Sqrt(Gx[i, j] * Gx[i, j] + Gy[i, j] * Gy[i, j]);
                 }
             }
         }
         //this assumes R=G=B, or can work for a specified channel, should write an overload
-        double[,] GradientDirection(Bitmap Gx, Bitmap Gy)
+        void GradientDirection(ref int[,] Gx, ref int[,] Gy)
         {
-            double[,] res = new double[Gx.Height, Gx.Width];
-            for (int i = 0; i < res.GetLength(0); i++)
+            angle = new int[Gx.GetLength(0), Gx.GetLength(1)];
+            for (int i = 0; i < angle.GetLength(0); i++)
             {
-                for (int j = 0; j < res.GetLength(1); j++)
+                for (int j = 0; j < angle.GetLength(1); j++)
                 {
                     //calculating angle as arctan(Gx,Gy) and converting to degrees by
-                    double rad = Math.Atan2(Gx.GetPixel(j, i).R, Gy.GetPixel(j, i).R) * (180 / Math.PI);
+                    double rad = (Math.Atan2(Gx[i, j], Gy[i, j]) * (180 / Math.PI)) % 180;
                     int direction;
-                    if ((rad >= -22.5 && rad < 22.5) || rad > 157.5 || rad <= -157.5)
+                    //vertical |
+                    //diagonal 1 /
+                    //horizontal --
+                    //diagonal 2 \
+                    if (rad > 0)
                     {
-                        direction = 0; //vertical |
-                    }
-                    else if ((rad >= 22.5 && rad < 67.5) || (rad > -157.5 && rad <= -112.5))
-                    {
-                        direction = 1; //diagonal 1 /
-                    }
-                    else if ((rad >= 67.5 && rad < 112.5) || (rad > -112.5 && rad <= -67.5))
-                    {
-                        direction = 2; //horizontal --
+                        if (rad < 22.5)
+                        {
+                            direction = 0;
+                        }
+                        else
+                        {
+                            if (rad < 67.5)
+                            {
+                                direction = 1;
+                            }
+                            else
+                            {
+                                if (rad < 112.5)
+                                {
+                                    direction = 2;
+                                }
+                                else
+                                {
+                                    if (rad < 157.5)
+                                    {
+                                        direction = 3;
+                                    }
+                                    else
+                                    {
+                                        direction = 0;
+                                    }
+                                }
+                            }
+                        }
                     }
                     else
                     {
-                        direction = 3; //diagonal 2 \
+                        if (rad > -22.5)
+                        {
+                            direction = 0;
+                        }
+                        else
+                        {
+                            if (rad > -67.5)
+                            {
+                                direction = 3;
+                            }
+                            else
+                            {
+                                if (rad > -112.5)
+                                {
+                                    direction = 2;
+                                }
+                                else
+                                {
+                                    if (rad > -157.5)
+                                    {
+                                        direction = 1;
+                                    }
+                                    else
+                                    {
+                                        direction = 0;
+                                    }
+                                }
+                            }
+                        }
                     }
-                    res[i, j] = direction;
+                    angle[i, j] = direction;
                 }
             }
-            return res;
         }
-        Bitmap Convolve(Bitmap target, ref int[,] filter)
+        //also add an overload for color channels
+        int[,] Convolve(ref Bitmap target, ref int[,] filter)
         {
-            Bitmap ret = new Bitmap(target);
+            int[,] ret = new int[target.Height, target.Width];
             for (int i = 0; i < target.Height; i++)
             {
                 for (int j = 0; j < target.Width; j++)
                 {
                     //here
                     int sumR = 0;
-                    int sumG = 0;
-                    int sumB = 0;
                     int offX = (filter.GetLength(0) - 1) / -2;
                     int offY = (filter.GetLength(1) - 1) / -2;
                     for (int a = filter.GetLength(0) - 1; a >= 0; a--)
@@ -129,11 +177,9 @@ namespace ImageProcessingCPU.Algorithms
                                 t = target.GetPixel(cj, ci);
                             }
                             sumR += t.R * filter[a, b];
-                            sumG += t.G * filter[a, b];
-                            sumB += t.B * filter[a, b];
                         }
                     }
-                    ret.SetPixel(j, i, Color.FromArgb(byte.MaxValue, (byte)Math.Clamp(sumR, 0, 255), (byte)Math.Clamp(sumG, 0, 255), (byte)Math.Clamp(sumB, 0, 255)));
+                    ret[i, j] = sumR;
                 }
             }
             return ret;
@@ -143,107 +189,62 @@ namespace ImageProcessingCPU.Algorithms
         //also works only with greyscale. consider overloading for color channels
         void NonMaxSuppression()
         {
-            //edges
-            //upper
-            for (int i = 1; i < angle.GetLength(1) - 1; i++)
-            {
-                if (angle[0, i] == 0)
-                {
-                    double p1 = Math.Max(gIntensity[0, i - 1], gIntensity[0, i + 1]);
-                    double p2 = gIntensity[0, i];
-                    if (p2 != Math.Max(p1, p2))
-                    {
-                        gIntensity[0, i] = 0;
-                    }
-                }
-            }
-            //lower
-            for (int i = 1; i < angle.GetLength(1) - 1; i++)
-            {
-                if (angle[angle.GetLength(0) - 1, i] == 0)
-                {
-                    double p1 = Math.Max(gIntensity[angle.GetLength(0) - 1, i - 1], gIntensity[angle.GetLength(0) - 1, i + 1]);
-                    double p2 = gIntensity[angle.GetLength(0) - 1, i];
-                    if (p2 != Math.Max(p1, p2))
-                    {
-                        gIntensity[angle.GetLength(0) - 1, i] = 0;
-                    }
-                }
-            }
-            //left
-            for (int i = 1; i < angle.GetLength(0) - 1; i++)
-            {
-                if (angle[i, 0] == 2)
-                {
-                    double p1 = Math.Max(gIntensity[i - 1, 0], gIntensity[i + 1, 0]);
-                    double p2 = gIntensity[i, 0];
-                    if (p2 < p1)
-                    {
-                        gIntensity[i, 0] = 0;
-                    }
-                }
-            }
-            //right
-            for (int i = 1; i < angle.GetLength(0) - 1; i++)
-            {
-                if (angle[i, angle.GetLength(1) - 1] == 2)
-                {
-                    double p1 = Math.Max(gIntensity[i - 1, angle.GetLength(1) - 1], gIntensity[i + 1, angle.GetLength(1) - 1]);
-                    double p2 = gIntensity[i, angle.GetLength(1) - 1];
-                    if (p2 < p1)
-                    {
-                        gIntensity[i, angle.GetLength(1) - 1] = 0;
-                    }
-                }
-            }
             //inner image
+            double[,] x = new double[gIntensity.GetLength(0), gIntensity.GetLength(1)];
             for (int i = 1; i < angle.GetLength(0) - 1; i++)
             {
                 for (int j = 1; j < angle.GetLength(1) - 1; j++)
                 {
-                    double p1;
-                    double p2;
                     switch (angle[i, j])
                     {
                         case 0:
-                            //vertical
-                            p1 = Math.Max(gIntensity[i - 1, j], gIntensity[i + 1, j]);
-                            p2 = gIntensity[i, j];
-                            if (p2 < p1)
+                            //vertical angle = horizontal edge
+                            if (gIntensity[i, j] < Math.Max(gIntensity[i + 1, j], gIntensity[i - 1, j]) * adj)
                             {
-                                gIntensity[i, j] = 0;
+                                x[i, j] = 0;
+                            }
+                            else
+                            {
+                                x[i, j] = gIntensity[i, j];
                             }
                             break;
                         case 1:
                             //diagonal 1
-                            p1 = Math.Max(gIntensity[i - 1, j + 1], gIntensity[i + 1, j - 1]);
-                            p2 = gIntensity[i, j];
-                            if (p2 < p1)
+                            if (gIntensity[i, j] < Math.Max(gIntensity[i - 1, j + 1], gIntensity[i + 1, j - 1]) * adj)
                             {
-                                gIntensity[i, j] = 0;
+                                x[i, j] = 0;
+                            }
+                            else
+                            {
+                                x[i, j] = gIntensity[i, j];
                             }
                             break;
                         case 2:
                             //horizontal 
-                            p1 = Math.Max(gIntensity[i, j - 1], gIntensity[i, j + 1]);
-                            p2 = gIntensity[i, j];
-                            if (p2 < p1)
+                            if (gIntensity[i, j] < Math.Max(gIntensity[i, j + 1], gIntensity[i, j - 1]) * adj)
                             {
-                                gIntensity[i, j] = 0;
+                                x[i, j] = 0;
+                            }
+                            else
+                            {
+                                x[i, j] = gIntensity[i, j];
                             }
                             break;
                         case 4:
                             //diagonal 2
-                            p1 = Math.Max(gIntensity[i - 1, j - 1], gIntensity[i + 1, j + 1]);
-                            p2 = gIntensity[i, j];
-                            if (p2 < p1)
+                            if (gIntensity[i, j] < Math.Max(gIntensity[i + 1, j + 1], gIntensity[i - 1, j - 1]) * adj)
                             {
-                                gIntensity[i, j] = 0;
+                                x[i, j] = 0;
+                            }
+                            else
+                            {
+                                x[i, j] = gIntensity[i, j];
                             }
                             break;
                     }
                 }
             }
+            gIntensity = x;
         }
         Bitmap ReconstructFromGradient()
         {
